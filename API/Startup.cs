519ee8a8b2fa25113d3ad1fb2656a75ece5809b1
Amir <hesamian@uwm.edu>
2020-09-08 00:00:51 -0500

    Optimized the imports

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Domainlogic;
+﻿using Domainlogic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Hosting;
-using Models;
 
 namespace API
 {
